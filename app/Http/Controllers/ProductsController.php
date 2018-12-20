448bd7441152dcf90e967aaf2488d2102aa31179
vagrant <vagrant@homestead>
2018-12-20 07:38:09 +0000

    前端-商品搜索和排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,8 +7,43 @@ use App\Models\Product;
 
 class ProductsController extends Controller
 {
-    public function index(){
-        $products = Product::where('on_sale', true)->paginate(16);
-        return view('products.index',['products'=>$products]);
+    public function index(Request $request){
+        // 创建查询构造器
+        $builder = Product::query()->where('on_sale', true);
+        // $builder = Product::where('on_sale', true);
+        // 判断用户是否使用了搜索(如果没有则略过代码块，如果有则赋值给变量$search)
+        if($search = $request->input('search', '')){
+            $like = '%'.$search.'%';
+            // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('sku', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+        // 判断是否有排序条件
+        if($order = $request->input('order', '')){
+            // 是否已_asc或者_desc结尾
+            if(preg_match('/^(.+)_(asc|desc)$/', $order, $res)){
+                // dd($res);  // array [0 => "rating_desc", 1 => "rating", 2 => "desc"]
+                if(in_array($res[1], ['price', 'rating', 'sold_count'] )){
+                    // 说明是合法的排序值
+                    $res = $builder->orderBy($res[1], $res[2]);
+                }
+            }
+        }
+        
+        $products = $builder->paginate(16);
+        return view('products.index',[
+            'products'=>$products,
+            // 优化搜索体验 搜索后搜索框不会变空白
+            'filters'=>[
+                'search' => $search,
+                'order' => $order
+            ]
+        ]);
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,24 +3,62 @@
 @section('title', '商品列表')
 
 @section('content')
-    <div class="row p-0">
+    <div class="row mb-2">
+        <form class="form-inline my-2 my-lg-0 w-100 search-form" action="{{ route('products.index') }}" method="get">
+            <div class="col-6 text-left">
+                <input class="form-control mr-sm-2" name="search" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </div>
+            <div class="col-6 text-right">
+                <select class="custom-select w-50" name="order">
+                    <option value="">排序</option>
+                    <option value="price_asc">价格从低到高</option>
+                    <option value="price_desc">价格从高到低</option>
+                    <option value="sold_count_desc">销量从高到低</option>
+                    <option value="sold_count_asc">销量从低到高</option>
+                    <option value="rating_desc">评价从高到低</option>
+                    <option value="rating_asc">评价从低到高</option>
+                </select>
+            </div>
+        </form>
+    </div>
+
+
+    <div class="row">
         @foreach($products as $product)
-        <div class="card-deck col-3 mb-3">
-                <div class="card">
+            <div class="card-deck col-md-3 col-sm-4 mb-3 p-0 m-0">
+                <div class="card p-1">
                     <img class="card-img-top border" src="{{ $product->image_url }}" alt="Card image cap">
-                    <div class="card-body text-center p-1">
-                        <div>
-                            <div class="card-title"><b>$</b>{{ $product->price }}</div>
+                    <div class="col card-body px-3 py-0">
+                        <div class="col">
+                            <div><b>$</b>{{ $product->price }}</div>
                             <div>{{ $product->title }}</div>
                         </div>
-                        <div class="px-3 mx-auto">
+                        <div class="col mx-auto">
                             <span class="float-left">销量:{{ $product->sold_count }}</span>
-                            <span class="float-right">评分:{{ $product->review_count }}</span>
+                            <span class="float-right">评分:{{ $product->rating }}</span>
                         </div>
                     </div>
                 </div>
             </div>
         @endforeach
-        <div class="pull-right">{{ $products->render() }}</div>
     </div>
+    <div class="float-right">
+        {{ $products->render() }}
+    </div>
+@endsection
+
+@section('customJS')
+    <script>
+        // Unexpected token & 报错  需要使用下面的方式哦~~~
+        var filters =  {!! json_encode($filters) !!}
+        $(document).ready(function (){
+            $('.search-form input[name=search]').val(filters.search);
+            $('.search-form select[name=order]').val(filters.order);
+
+            $('.search-form select[name=order]').change(function (){
+                $('.search-form').submit();
+            })
+        })
+    </script>
 @endsection
